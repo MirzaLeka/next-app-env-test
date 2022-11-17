fc838a20221aa31902b9d8e2caa57fc8b18dc70b
mirzaleka <mirza_376@hotmail.com>
2022-11-17 11:05:16 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+// Next.js can read env variables from .env file without dotenv or similar lib
+console.log('process.env.APP_HOME [nextjs_config] :>> ', process.env.APP_HOME);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: { // needs to be put here in order to be accessible by Client side
+    APP_HOME: process.env.APP_HOME
+  }
 }
 
 module.exports = nextConfig
